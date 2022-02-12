5715eed9245fcefe6fc28a30156ba3cae009f25c
Matheus Sant'ana <matheusm.santana@outlook.com>
2022-02-12 14:59:18 -0300

    Done adding a swagger documentation.

--- PlanetlyProject/urls.py
+++ PlanetlyProject/urls.py
@@ -14,10 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentation",
+      default_version='v1',
+      description="Planetly documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -25,5 +40,8 @@ urlpatterns = [
     path('', include('carbon_usage.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
